1eb3bcaa36b9e6c37ae4a50ab3f843877c98ec34
Doak Covington <doakcovington@protonmail.com>
2019-11-28 21:57:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,11 +5,9 @@
 def fizzbuzz(n)
   if n % 3 == 0 && n % 5 == 0
     "FizzBuzz"
-  elsif n % 5 == 0
-     "Buzz"
   elsif n % 3 == 0
     "Fizz"
-  else
-    return nil
+  elsif n % 5 == 0
+     "Buzz"
   end
 end
